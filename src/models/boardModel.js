636f43ce3608a3e86786e54e7463d98bd04a9e05
MinaPhan2815 <minhnhatphan2815@gmail.com>
2025-02-14 09:36:22 +0700

    Add service and model layer

---
+++ src/models/boardModel.js
@@ -0,0 +1,36 @@
+import Joi from 'joi'
+import { GET_DB } from '~/config/mongodb'
+
+const BOARD_COLLECTION_NAME = 'boards'
+const BOARD_COLLECTION_SCHEMA = Joi.object({
+  title: Joi.string().min(3).max(50).required().trim().strict(),
+  description: Joi.string().min(3).max(256).required().trim().strict(),
+  slug: Joi.string().min(3).required().trim().strict(),
+
+  columnOrderIds: Joi.array().items(Joi.string()).default([]),
+  createdAt: Joi.date().timestamp('javascript').default(Date.now),
+  updatedAt: Joi.date().timestamp('javascript').default(null),
+  _destroy: Joi.boolean().default(false)
+})
+
+const createNew = async (data) => {
+  try {
+    return await GET_DB().collection(BOARD_COLLECTION_NAME).insertOne(data)
+  } catch (error) { throw new Error(error) }
+}
+
+const findOneById = async (id) => {
+  try {
+    const result = await GET_DB().collection(BOARD_COLLECTION_NAME).findOne({
+      _id: id
+    })
+    return result
+  } catch (error) { throw new Error(error) }
+}
+
+export const boardModel = {
+  BOARD_COLLECTION_NAME,
+  BOARD_COLLECTION_SCHEMA,
+  createNew,
+  findOneById
+}
---
+++ src/services/boardService.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-useless-catch */
+import { boardModel } from '~/models/boardModel'
+import { slugify } from '~/utils/formatters'
+
+const createNew = async (reqBody) => {
+  try {
+    const newBoard = {
+      ...reqBody,
+      slug: slugify(reqBody.title)
+    }
+
+    const createdBoard = await boardModel.createNew(newBoard)
+    const getNewBoard = await boardModel.findOneById(createdBoard.insertedId)
+
+    // service always must return a value
+    return getNewBoard
+  } catch (error) {
+    throw error
+  }
+}
+
+export const boardService = {
+  createNew
+}
